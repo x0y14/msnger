57c0d00ce4d2633ff1d6aaafdcf741139c0b1c51
x0y14 <ya01ma08@gmail.com>
2021-12-23 23:43:20 +0900

    service実装

--- cmd/account_service/server/server_test.go
+++ cmd/account_service/server/server_test.go
@@ -6,33 +6,65 @@ import (
 	"github.com/x0y14/msnger/pkg/service/account"
 	"google.golang.org/grpc/metadata"
 	"log"
+	"testing"
 )
 
-func main() {
+const (
+	testUserEmail    = "test001@example.com"
+	testUserPassword = "12345"
+	testUserId       = "Uc727m0is1s46dk46ivr0"
+	testUserToken    = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJVYzcyN20waXMxczQ2ZGs0Nml2cjAifQ.MtWzvkXjEk5R_4HLs6gwAH-Hopcc8UWq2Go6tHRrnHI"
+)
+
+func TestCreateAccount(t *testing.T) {
 	cl := *account.CreateClient("localhost:9292")
 
-	// login必要ない
-	resultOfLogin, err := cl.Login(context.Background(), &pb.LoginRequest{Email: "sample@example.com", Password: "p@ssword"})
+	result, err := cl.CreateAccount(context.Background(), &pb.CreateAccountRequest{
+		Email:    testUserEmail,
+		Password: testUserPassword,
+	})
 	if err != nil {
-		log.Fatalf("failed to login: %v", err)
-	}
-	myUserId := resultOfLogin.GetUserId()
-	myToken := resultOfLogin.GetJwt()
-	log.Printf("I got UserId: %v, Token: %v\n", myUserId, myToken)
-
-	// 失敗: loginなし
-	resultOfGetEmail, err := cl.GetEmail(context.Background(), &pb.GetEmailRequest{})
-	if err != nil {
-		log.Printf("failed to get email: %v", err)
+		t.Fatalf("failed to create an account: %v", err)
 	}
 
-	// 成功: loginあり
-	bearer := "Bearer " + myToken
-	md := metadata.New(map[string]string{"authorization": bearer})
+	log.Printf("UserId: %v\n", result.GetUserId())
+	log.Printf("Token: %v\n", result.GetJwt())
+}
+
+func TestLogin(t *testing.T) {
+	cl := *account.CreateClient("localhost:9292")
+	result, err := cl.Login(context.Background(), &pb.LoginRequest{
+		Email:    testUserEmail,
+		Password: testUserPassword,
+	})
+	if err != nil {
+		t.Fatalf("failed to login: %v", err)
+	}
+
+	log.Printf("UserId: %v\n", result.GetUserId())
+	log.Printf("Token: %v\n", result.GetJwt())
+}
+
+func TestGetEmail_NoLogin(t *testing.T) {
+	cl := *account.CreateClient("localhost:9292")
+	result, err := cl.GetEmail(context.Background(), &pb.GetEmailRequest{})
+	if err != nil {
+		t.Fatalf("failed to get email: %v", err)
+	}
+	log.Printf("Email: %v", result.Email)
+}
+
+func TestGetEmail_Login(t *testing.T) {
+	cl := *account.CreateClient("localhost:9292")
+
+	bearer := "Bearer " + testUserToken
+
+	md := metadata.Pairs("authorization", bearer)
 	ctx := metadata.NewOutgoingContext(context.Background(), md)
-	resultOfGetEmail, err = cl.GetEmail(ctx, &pb.GetEmailRequest{})
+
+	result, err := cl.GetEmail(ctx, &pb.GetEmailRequest{})
 	if err != nil {
-		log.Fatalf("failed to get email: %v", err)
+		t.Fatalf("failed to get email: %v", err)
 	}
-	log.Printf("success to get email: %v\n", resultOfGetEmail.GetEmail())
+	log.Printf("Email: %v", result.Email)
 }
--- pkg/service/account/server.go
+++ pkg/service/account/server.go
@@ -3,10 +3,12 @@ package account
 import (
 	"context"
 	"github.com/x0y14/msnger/pkg/auth"
+	"github.com/x0y14/msnger/pkg/db"
 	"github.com/x0y14/msnger/pkg/misc"
 	"github.com/x0y14/msnger/pkg/protobuf"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"log"
 )
 
 type ServiceServer struct {
@@ -30,10 +32,24 @@ func (s *ServiceServer) AuthFuncOverride(ctx context.Context, fullMethodName str
 
 func (s *ServiceServer) CreateAccount(ctx context.Context, req *protobuf.CreateAccountRequest) (*protobuf.CreateAccountResult, error) {
 	newUserId := misc.GenerateUserId()
+
+	// store account data to db
+	err := db.InsertAccount(&db.InsertAccountReq{
+		Id:       newUserId,
+		Email:    req.GetEmail(),
+		Password: req.GetPassword(),
+	})
+	if err != nil {
+		log.Printf("[Account] CreateAccount Err: %v\n", err)
+		return nil, status.Errorf(codes.Internal, "error")
+	}
+
+	// generate token
 	token, err := auth.GenerateJWTToken(newUserId)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to generate token.")
 	}
+
 	return &protobuf.CreateAccountResult{
 		UserId: newUserId,
 		Jwt:    token,
@@ -41,20 +57,40 @@ func (s *ServiceServer) CreateAccount(ctx context.Context, req *protobuf.CreateA
 }
 
 func (s *ServiceServer) Login(ctx context.Context, req *protobuf.LoginRequest) (*protobuf.LoginResult, error) {
-	if req.Email != "sample@example.com" || req.Password != "p@ssword" {
-		return nil, status.Errorf(codes.Unauthenticated, "failed to login, email or password was invalid")
+	account, err := db.SelectAccountWithEmail(&db.SelectAccountReq{Email: req.GetEmail()})
+	if err != nil {
+		log.Printf("[Account] Login Err: %v\n", err)
+		return nil, status.Errorf(codes.Internal, "error")
 	}
-	sampleUserId := misc.GenerateUserId()
-	token, err := auth.GenerateJWTToken(sampleUserId)
+
+	// アカウント不明
+	if account == nil {
+		return nil, status.Errorf(codes.Unauthenticated, "user not found.")
+	}
+
+	// 成功, トークン生成
+	jwt, err := auth.GenerateJWTToken(account.GetId())
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to generate token.")
 	}
+
 	return &protobuf.LoginResult{
-		UserId: sampleUserId,
-		Jwt:    token,
+		UserId: account.GetId(),
+		Jwt:    jwt,
 	}, nil
 }
 
 func (s *ServiceServer) GetEmail(ctx context.Context, req *protobuf.GetEmailRequest) (*protobuf.GetEmailResult, error) {
-	return &protobuf.GetEmailResult{Email: "sample@example.com"}, nil
+	userId := ctx.Value("userId").(string)
+	account, err := db.SelectAccountWithId(&db.SelectAccountReq{Id: userId})
+	if err != nil {
+		log.Printf("[Account] GetEmail Err: %v\n", err)
+		return nil, status.Errorf(codes.Internal, "error")
+	}
+
+	if account == nil {
+		return nil, status.Errorf(codes.Unauthenticated, "user not found.")
+	}
+
+	return &protobuf.GetEmailResult{Email: account.Email}, nil
 }

04faf5263a87be1263dd0e52e9f5792bbc6c3f5c
x0y14 <ya01ma08@gmail.com>
2021-12-23 17:59:30 +0900

    clientに記述されていたserverのテストを、server_testに移動した。

--- cmd/account_service/client/client.go
+++ cmd/account_service/client/client.go
@@ -1,38 +1,4 @@
 package main
 
-import (
-	"context"
-	pb "github.com/x0y14/msnger/pkg/protobuf"
-	"github.com/x0y14/msnger/pkg/service/account"
-	"google.golang.org/grpc/metadata"
-	"log"
-)
-
 func main() {
-	cl := *account.CreateClient("localhost:9292")
-
-	// login必要ない
-	resultOfLogin, err := cl.Login(context.Background(), &pb.LoginRequest{Email: "sample@example.com", Password: "p@ssword"})
-	if err != nil {
-		log.Fatalf("failed to login: %v", err)
-	}
-	myUserId := resultOfLogin.GetUserId()
-	myToken := resultOfLogin.GetJwt()
-	log.Printf("I got UserId: %v, Token: %v\n", myUserId, myToken)
-
-	// 失敗: loginなし
-	resultOfGetEmail, err := cl.GetEmail(context.Background(), &pb.GetEmailRequest{})
-	if err != nil {
-		log.Printf("failed to get email: %v", err)
-	}
-
-	// 成功: loginあり
-	bearer := "Bearer " + myToken
-	md := metadata.New(map[string]string{"authorization": bearer})
-	ctx := metadata.NewOutgoingContext(context.Background(), md)
-	resultOfGetEmail, err = cl.GetEmail(ctx, &pb.GetEmailRequest{})
-	if err != nil {
-		log.Fatalf("failed to get email: %v", err)
-	}
-	log.Printf("success to get email: %v\n", resultOfGetEmail.GetEmail())
 }
